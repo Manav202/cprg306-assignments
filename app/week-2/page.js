ab0a058c054a1eb91e1e9a64a1093232b3eba2d9
Manav202 <141389168+Manav202@users.noreply.github.com>
2024-06-04 17:45:41 -0600

    Assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StudentInfo from './student-info';
+ 
+const Page = () => {
+    return (
+        <div>
+            <h1>Week 2 Assignment</h1>
+            <StudentInfo />
+        </div>
+       
+    );
+}
+ 
+export default Page;
