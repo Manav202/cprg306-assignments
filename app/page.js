98eee1f14f64b78abd1d866d9b470ded793cb64d
Manav202 <141389168+Manav202@users.noreply.github.com>
2024-07-16 15:13:17 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,33 +1,17 @@
-import React from 'react';
 import Link from 'next/link';
- 
-const Home = () => {
+
+export default function Home() {
   return (
-    <div className='text-white'>
+    <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p> <Link href="/week-2">
-        Go to Week 2 Assignments
-      </Link></p>
-      <p><Link href="/week-3">
-        Go to Week 3 Assignments
-      </Link></p>
-      <p><Link href="/week-4">
-        Go to Week 4 Assignments
-      </Link></p>
-      <p><Link href="/week-5">
-        Go to Week 5 Assignments
-      </Link></p>
-      <p><Link href="/week-6">
-        Go to Week 6 Assignments
-      </Link></p>
-      <p><Link href="/week-7">
-        Go to Week 7 Assignments
-      </Link></p>
-     <p><Link href="/week-8">
-    Go to Week 8 Assignments
-  </Link></p>
-</div>
+      <p><Link href="/week-2">Week 2 Assignment</Link></p>
+      <p><Link href="/week-3">Week 3 Assignment</Link></p>
+      <p><Link href="/week-4">Week 4 Assignment</Link></p>
+      <p><Link href="/week-5">Week 5 Assignment</Link></p>
+      <p><Link href="/week-6">Week 6 Assignment</Link></p>
+      <p><Link href="/week-7">Week 7 Assignment</Link></p>
+      <p><Link href="/week-8">Week 8 Assignment</Link></p>
+      <p><Link href="/week-10">Week 10 Assignment</Link></p>
+    </main>
   );
 }
- 
-export default Home;
